c5e062333bd5b39f3ac59b2ed26206d3533d5226
Cypher890 <abdulsuhaib07@gmail.com>
2022-06-20 15:29:00 +0530

    edited trueorfalse

--- JavaScript/trueOrFalse.js
+++ JavaScript/trueOrFalse.js
@@ -1,7 +1,19 @@
-const userInput = prompt("enter something");
+// const userInput = prompt("enter something");
 
-if (userInput){
-    console.log("TRUTHY!")
+// if (userInput){
+//     console.log("TRUTHY!")
+// }else{
+//     console.log("FALSY!")
+// }
+
+const age = 3;
+if(age >= 0 && age < 5 || age >= 65){
+    console.log("free");
+}else if (age >= 5 && age < 10){
+    console.log("$10");
+}else if (age >= 10 && age <65){
+    console.log("$65");
 }else{
-    console.log("FALSY!")
+    console.log("Invalid age");
+    console.log(age);
 }
