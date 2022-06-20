d62cb6050b36f0d0b4b544603eae37374cdbee5d
Cypher890 <abdulsuhaib07@gmail.com>
2022-06-20 15:04:30 +0530

    changed passwd to and methode

--- JavaScript/passwd.js
+++ JavaScript/passwd.js
@@ -1,15 +1,23 @@
-const password = prompt('please enter your password')
+// const password = prompt('please enter your password')
 
-if (password.length >=6){
+// if (password.length >=6){
 
-    if(password.indexOf(' ') === -1) {
-        console.log("Valid Passsword!")
-    }else{
-        console.log("Passsword cannot contain spaces")
+//     if(password.indexOf(' ') === -1) {
+//         console.log("Valid Passsword!")
+//     }else{
+//         console.log("Passsword cannot contain spaces")
 
-    }
+//     }
 
 
+// } else{
+//     console.log("Passsword too short! Must be 6+ character")
+// }
+
+//same program using and nethode
+const password = prompt("Enter your password");
+if (password.length >=6 && password.indexOf(' ') === -1) {
+    console.log("valid Passsword")
 } else{
-    console.log("Passsword too short! Must be 6+ character")
+    console.log("invalid Passsword")
 }
