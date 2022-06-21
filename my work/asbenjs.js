631cc90f26df058e055d97ddbd4341f908edf6a0
Cypher890 <abdulsuhaib07@gmail.com>
2022-06-21 19:54:19 +0530

    started asbeneh with ulfric

---
+++ my work/asbenjs.js
@@ -0,0 +1,12 @@
+let firstName = 'ulfric'
+let lastName = 'storms'
+let titleName ='High King'
+let city = 'windhelm'
+let race = 'nord'
+
+const nature = 'ruthless'
+const role = 'warrior'
+const magicka = 4
+const stamina = 10
+
+let hold = 'hjilmarch', group = 'stormcloak', slogan = 'skyrimForNords'
---
+++ my work/ulfric.js
@@ -0,0 +1,3 @@
+console.log(firstName,lastName,titleName,city,race)
+console.log(nature,role,magicka,stamina)
+console.log(hold,group,slogan)
