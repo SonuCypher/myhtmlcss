9dcba39c59ba93ffb59885717791fa5addf80665
Cypher890 <abdulsuhaib07@gmail.com>
2022-06-23 19:01:08 +0530

    updated this methodes

--- JavaScript/MethodJsci.js
+++ JavaScript/MethodJsci.js
@@ -1,10 +1,19 @@
-const person = {
-    first: 'John',
-    last: 'Herjavec',
-    fullName(){
-        return `${this.first} ${this.last}`
+// const person = {
+//     first: 'John',
+//     last: 'Herjavec',
+//     fullName(){
+//         return `${this.first} ${this.last}`
+//     }
+// }
+// person.fullName()
+// person.last = 'plant'
+// person.fullName()
+
+const cat = {
+    name:'blue Steele',
+    color:'grey',
+    breed:'scottish fold',
+    meow(){
+        console.log(`${this.name} says meoeew`)
     }
 }
-person.fullName()
-person.last = 'plant'
-person.fullName()
