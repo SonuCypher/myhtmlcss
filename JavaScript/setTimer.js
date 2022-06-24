e4c657dbba05c5bb01128eca312ca5bc372e1157
Cypher890 <abdulsuhaib07@gmail.com>
2022-06-24 12:52:27 +0530

    added timer

---
+++ JavaScript/setTimer.js
@@ -0,0 +1,13 @@
+// console.log('hello')
+// setTimeout(() => {
+//     console.log("...are you there?")
+// }, 5000)
+
+// console.log("GOODBYE!")
+
+
+ const id= setInterval(()=>{
+    console.log(Math.random())
+    console.log("hi")
+},2000)
+// use clearInterval to clear the timer
