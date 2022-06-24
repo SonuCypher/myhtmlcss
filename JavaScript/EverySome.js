d161d0630862e7f57bfbeb9a982ab6ee2e58cab6
Cypher890 <abdulsuhaib07@gmail.com>
2022-06-24 15:08:38 +0530

    added some

---
+++ JavaScript/EverySome.js
@@ -0,0 +1,7 @@
+const movies=[{title:'doctorstrange',score:'6.1'},
+              {title:'superman',score:'3.2'},
+              {title:'batman',score:'9.2'},
+              {title:'spiderman',score:'9.2'},
+            ]
+
+            movies.some(movie => movie.title === title)
