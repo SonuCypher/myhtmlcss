e59bb90149a7295681f04ca20fdb22d85b573bbf
Cypher890 <abdulsuhaib07@gmail.com>
2022-06-30 12:04:41 +0530

    added buttnEvent

---
+++ JavaScript/DOM/buttonEvent.js
@@ -0,0 +1,7 @@
+document.querySelector('button').addEventListener('click', function(eve){
+    console.log(eve)
+})
+document.querySelector('input').addEventListener('keydown',function(evt){
+console.log('KEYDOWN')
+console.log(evt)
+})
