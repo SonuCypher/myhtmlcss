b660a16b197edd1f61121c13d4a96a768d9b694c
Cypher890 <abdulsuhaib07@gmail.com>
2022-06-30 18:51:59 +0530

    added inptChangEve

---
+++ JavaScript/DOM/inptChngEve.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('input')
+const h1= document.querySelector('h1')
+
+// input.addEventListener('change', function (e){
+//     console.log("input changed")
+// })
+// input.addEventListener('input', function (e){
+//     console.log('input event')
+// })
+input.addEventListener('input', function (e){
+    h1.innerText = "welcome"+" "+input.value
+})
