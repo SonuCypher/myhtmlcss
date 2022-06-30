80c94efdf5fb462e5d3430eb05a9a9232661cd53
Cypher890 <abdulsuhaib07@gmail.com>
2022-06-30 13:48:15 +0530

    updated form event

---
+++ JavaScript/DOM/formEvent.js
@@ -0,0 +1,11 @@
+const form =document.querySelector('#twitt')
+const input =document.querySelector('#inputI')
+const list =document.querySelector('#cats')
+form.addEventListener("submit", function(e){
+    e.preventDefault()
+    const catName = input.value
+    const newList = document.createElement('li')
+    newList.innerText = catName
+    list.append(newList)
+    input.value = ""
+})
