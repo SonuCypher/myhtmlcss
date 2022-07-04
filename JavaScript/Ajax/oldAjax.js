d062c6ad78d95d6223958d9936d44683f2918186
Cypher890 <abdulsuhaib07@gmail.com>
2022-07-04 21:05:26 +0530

    added old methode of fetch

---
+++ JavaScript/Ajax/oldAjax.js
@@ -0,0 +1,20 @@
+//"https://www.dnd5eapi.co/api/monsters/adult-black-dragon"
+
+
+/*const req = new XMLHttpRequest()
+
+req.onload = function () {
+    console.log('it loaded')
+    const data = JSON.parse(this.responseText)
+    console.log(data.name, data.alignment)
+}
+
+req.onerror = function (){
+    console.log("error loading")
+    console.log(this)
+
+}
+
+req.open('GET',"https://www.dnd5eapi.co/api/monsters/adult-black-dragon")
+req.send()*/
+
