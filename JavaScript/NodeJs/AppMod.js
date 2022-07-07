cd3809d6681b9dcbb48c0849ac128458d3a0053d
Cypher890 <abdulsuhaib07@gmail.com>
2022-07-07 18:29:34 +0530

    added AppMod,MathMod,Intro

---
+++ JavaScript/NodeJs/AppMod.js
@@ -0,0 +1,4 @@
+const math = require('./MathMod')
+console.log(math.pie)
+console.log(math.sum(4,5))
+console.log(math.mult(5))
--- JavaScript/NodeJs/intro.js
+++ JavaScript/NodeJs/intro.js
@@ -1,1 +1,11 @@
-console.log("hello starting node")
+// use node filename argument (eg:-node intro.js hi )
+
+const args = process.argv.slice(2)
+for (let arg of args ){
+    console.log("hello "+arg)
+}
+
+
+
+
+
