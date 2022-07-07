3dc663a262bfdcebafd054fa5cc8c9185bd16057
Cypher890 <abdulsuhaib07@gmail.com>
2022-07-07 19:20:43 +0530

    destructured AppMod and MathMod

--- JavaScript/NodeJs/AppMod.js
+++ JavaScript/NodeJs/AppMod.js
@@ -1,4 +1,8 @@
-const math = require('./MathMod')
-console.log(math.pie)
+// const math = require('./MathMod')
+
+/*console.log(math.pie)
 console.log(math.sum(4,5))
-console.log(math.mult(5))
+console.log(math.mult(5))*/
+const { pie,mult}= require('./MathMod')
+console.log(pie)
+console.log(mult(9))
--- JavaScript/NodeJs/MathMod.js
+++ JavaScript/NodeJs/MathMod.js
@@ -4,6 +4,13 @@ const PI = 3.14159
 
 const square = x => x * x
 
-module.exports.sum = add
+ const math = {
+     sum: add,
+     pie: PI,
+     mult: square
+ }
+
+ module.exports = math
+/*module.exports.sum = add
 module.exports.pie = PI
-module.exports.mult = square
+module.exports.mult = square*/
