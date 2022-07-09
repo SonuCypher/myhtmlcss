8a4c5bf27b486b36847366f3c086f23f1d8b44f2
Cypher890 <abdulsuhaib07@gmail.com>
2022-07-09 11:14:25 +0530

    added asabeneh Day 2

---
+++ Asbenah/Day2/challenge.js
@@ -0,0 +1,3 @@
+let challenge = '     30 Days OF JavaScript   '
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+let Because ='You cannot end a sentence with because because because is a conjunction'
