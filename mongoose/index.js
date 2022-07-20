38c5afdf04223ae480d41d8ef15c5a33f1ab50d0
Cypher890 <abdulsuhaib07@gmail.com>
2022-07-20 22:23:29 +0530

    added mangoose update

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,10 +1,25 @@
-// const mongoose= require('mongoose')
-// mongoose.connect('mongodb://localhost:27017/movieApp')
+// const mongoose = require('mongoose');
 
-const mongoose = require('mongoose');
+// main().catch(err => console.log(err));
 
-main().catch(err => console.log(err));
+// async function main() {
+//   await mongoose.connect('mongodb://localhost:27017/movieApp');
+// }
+const mongoose= require('mongoose')
+ mongoose.connect('mongodb://localhost:27017/movieApp')
+ .then(()=>{
+  console.log("CONNECTION OPEN!!!")
+ })
+ .catch(err => {
+    console.log("OH NO ERROR")
+    console.log(err)
+ })
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/movieApp');
-}
+ const movieSchema = new mongoose.Schema({
+  title:String,
+  year:Number,
+  score:Number,
+  rating:String
+ })
+ const Movie = mongoose.model('Movie', movieSchema)
+ const amadeus = new Movie({title : 'Amadeus', year :1986, score : 9.2, rating :'R'})
