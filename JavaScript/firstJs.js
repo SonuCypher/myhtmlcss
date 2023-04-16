2b99aa932a8012136c76f6ca09eae822210d8ed7
Cypher890 <abdulsuhaib07@gmail.com>
2023-04-16 21:45:46 +0530

    edited the emmet html file

--- JavaScript/firstJs.js
+++ JavaScript/firstJs.js
@@ -1,10 +1,20 @@
-let random=Math.random();
-if(random<0.5){
-    console.log("your number is less than 0.5");
-    console.log(random);
+// let random=Math.random();
+// if(random<0.5){
+//     console.log("your number is less than 0.5");
+//     console.log(random);
+// }
+
+// if(random >= 0.5){
+//     console.log("your number is greater than 0.5!!");
+//     console.log(random);
+// }
+
+const sum = () => {
+    return(
+        4+4
+    ) 
 }
 
-if(random >= 0.5){
-    console.log("your number is greater than 0.5!!");
-    console.log(random);
-}
+const num = sum()
+
+console.log(num)
