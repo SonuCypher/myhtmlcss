69e019f8b98cd6ae696c0c7aba4bbcd6f126ff78
Cypher890 <abdulsuhaib07@gmail.com>
2023-05-14 19:22:25 +0530

    practising in asbenah

--- my work/ulfric.js
+++ my work/ulfric.js
@@ -1,3 +1,11 @@
-console.log(firstName,lastName,titleName,city,race)
-console.log(nature,role,magicka,stamina)
-console.log(hold,group,slogan)
+// console.log(firstName,lastName,titleName,city,race)
+// console.log(nature,role,magicka,stamina)
+// console.log(hold,group,slogan)
+const languages = ['english','finnish','finnish','french']
+const info = new Set(languages)
+info.add('Malayalam')
+console.log(info)
+
+let a = '123456'
+let A = [a]
+console.log(A)
